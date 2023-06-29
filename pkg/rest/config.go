0f02b3fc6c3230b84313ba92d8a49de3f4236c35
Bruno LEHOUQUE <bruno.lehouque@atos.net>
2023-06-29 11:16:26 +0200

    Add oauth2 config.

--- pkg/rest/config.go
+++ pkg/rest/config.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"github.com/eflows4hpc/hpcwaas-api/pkg/managers/a4c"
 	"github.com/eflows4hpc/hpcwaas-api/pkg/managers/vault"
+	"golang.org/x/oauth2"
 )
 
 const DefaultListenAddress = "0.0.0.0:9090"
@@ -15,7 +16,9 @@ type Config struct {
 }
 
 type AuthConfig struct {
+	AuthType  string           `mapstructure:"auth_type,omitempty"`
 	BasicAuth *BasicAuthConfig `mapstructure:"basic_auth,omitempty"`
+	OAuth     *oauth2.Config
 }
 
 type BasicAuthConfig struct {
